639f7230168592cee340044e7fae5dbe8feb67b4
Matt Bartlett <info@mattbartlett.net>
2019-10-13 13:34:36 +0000

    Basic Playlist Resource

---
+++ src/Resource.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Spotify;
+
+use Spotify\Http\Request;
+use Spotify\Auth\Manager;
+
+/**
+ * Class Resource
+ *
+ * @package Spotify
+ */
+class Resource
+{
+    protected const API_BASE_URL = 'https://api.spotify.com/v1';
+
+    /**
+     * @var \Spotify\Http\Request
+     */
+    protected $request;
+
+    /**
+     * @var \Spotify\Auth\Manager
+     */
+    protected $manager;
+
+    /**
+     * @param \Spotify\Http\Request $request
+     * @param \Spotify\Auth\Manager $manager
+     */
+    public function __construct(Request $request, Manager $manager)
+    {
+        $this->manager = $manager;
+        $this->request = $request;
+    }
+
+    /**
+     * Fetch an access token from the Auth Manager.
+     *
+     * @return string
+     */
+    protected function getAccessToken() : string
+    {
+        return $this->manager->getAccessToken();
+    }
+}
---
+++ src/Resources/Playlist.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Spotify\Resources;
+
+use stdClass;
+use Spotify\Resource;
+use Spotify\Http\Request;
+
+/**
+ * Class Playlist
+ *
+ * @package Spotify\Resources
+ */
+class Playlist extends Resource
+{
+    /**
+     * Get a playlist owned by a Spotify user.
+     *
+     * @param string $playlist
+     *
+     * @return stdClass
+     */
+    public function getPlaylist(string $playlist) : stdClass
+    {
+        $url = sprintf('%s/playlists/%s', self::API_BASE_URL, $playlist);
+
+        return $this->request->get($url, [
+            'Authorization' => sprintf('Bearer %s', $this->getAccessToken()),
+        ]);
+    }
+
+    /**
+     * Get full details of the tracks of a playlist owned by a Spotify user.
+     *
+     * @param string $playlist
+     *
+     * @return stdClass
+     */
+    public function getPlaylistTracks(string $playlist) : stdClass
+    {
+        $url = sprintf('%s/playlists/%s/tracks', self::API_BASE_URL, $playlist);
+
+        return $this->request->get($url, [
+            'Authorization' => sprintf('Bearer %s', $this->getAccessToken()),
+        ]);
+    }
+}
