4d120f4fd859c3c5425d4c0b19931ba01bf2291b
Matt Bartlett <info@mattbartlett.net>
2020-02-16 14:16:24 +0000

    Docblocks

--- src/Exceptions/AuthenticationException.php
+++ src/Exceptions/AuthenticationException.php
@@ -4,7 +4,13 @@ namespace Spotify\Exceptions;
 
 class AuthenticationException extends \Exception
 {
+    /**
+     * @var int
+     */
     protected $code = 401;
 
+    /**
+     * @var string
+     */
     protected $message = 'Your access token is invalid or has expired.';
 }
--- src/Exceptions/UserRequiresAuthorizationException.php
+++ src/Exceptions/UserRequiresAuthorizationException.php
@@ -4,7 +4,13 @@ namespace Spotify\Exceptions;
 
 class UserRequiresAuthorizationException extends \Exception
 {
+    /**
+     * @var int
+     */
     protected $code = 403;
 
+    /**
+     * @var string
+     */
     protected $message = 'User needs to authorize.';
 }
